d5e5db0c36c79e11def7ff8e2c0fb457967f044b
marlon172004 <129074600+marlon172004@users.noreply.github.com>
2023-10-26 01:19:19 -0500

    listas de python
    soy muy lindo

---
+++ listas/eje4.py
@@ -0,0 +1,16 @@
+aprendices = []
+edades = []
+
+nombres_edades = [
+    ["Sebastian Tovar", 17],["Juan Mahecha", 17],["Maria Buenaventura", 17],["Mathew Guarnizo", 17],["Kevin Hernan", 17],["Melissa Lopez", 16],
+    ["Vanesa Ladino", 17],["Stiven Ramirez", 18],["Esteban Prada", 17],["Kevin Botero", 17],["Sebastian Pinzon", 17],["Camila Alape", 18],["Kevin Londoño", 23],
+    ["Nicolas Fierro", 18],["Santiago Gomez", 17],["Marlon Lopez", 19],["Stiwar Perez", 18],["Miguel Bernal", 17], ["Maria Molano", 18],["Nicolas Paulo", 17],
+    ["Laura Benavides", 17],["Wilfrank", 18],["Dahiana", 18],["Lina Barrios", 18],["Maria jose", 18],["Nataly", 17],["Matias Guzman", 17],["cristian Peña", 17],
+    ["Paula Garcia", 20]
+]
+# Contar cuántos aprendices tienen 18 años
+for nombre, edad in nombres_edades:
+    aprendices.append(nombre)
+    edades.append(edad)
+conteo_18_anios = edades.count(18)
+print("Cantidad de aprendices con 18 años:", conteo_18_anios)
