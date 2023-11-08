4637c820ee9851a7ff0551caa0e7532058c294b5
marlon172004 <129074600+marlon172004@users.noreply.github.com>
2023-11-07 21:12:58 -0500

    propuesto
    jajaja

--- diccionarios/propuesto.py
+++ diccionarios/propuesto.py
@@ -16,28 +16,4 @@ def agregarNovedad(idEquipo, fecha, descripcion):
     else:
         print(f'El equipo con ID {idEquipo} no existe.')
 
-# Función para buscar un equipo por su ID.
-def buscarEquipo(idEquipo):
-    if idEquipo in equipos:
-        print(f'Información del equipo con ID {idEquipo}:')
-        print(equipos[idEquipo])
-    else:
-        print(f'El equipo con ID {idEquipo} no existe.')
 
-# Función para mostrar un reporte de equipos con novedades.
-def mostrar_reporte_novedades():
-    for idEquipo, equipo in equipos.items():
-        if 'novedades' in equipo:
-            print(f'ID del equipo: {idEquipo}')
-            for novedad in equipo['novedades']:
-                print(f'Fecha: {novedad["fecha"]}')
-                print(f'Descripción: {novedad["descripcion"]}')
-            print('---')
-
-# Aquí puedes agregar más funciones para modificar y eliminar equipos, si lo necesitas.
-
-# Ejemplo de uso:
-agregarEquipo('001', 'Laptop', True, True)
-agregarNovedad('001', '2023-11-07', 'Pantalla rota')
-buscarEquipo('001')
-mostrar_reporte_novedades()
