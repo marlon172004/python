5811f743fda3bdce1b451556f7d338852a54ae3c
marlon172004 <129074600+marlon172004@users.noreply.github.com>
2023-11-23 09:39:07 -0500

    coemtado
    comentacion del codigo

--- Evaluacion/eva2.py
+++ Evaluacion/eva2.py
@@ -1,17 +1,22 @@
 class Votacion:
     def __init__(self):
-        self.voceros = set()
-        self.candidatos = {"Marlon": 0, "sandra": 0, "Stiwar": 0, "Blanco": 0}
-        self.votosRealizados = set()
+        # Inicialización de variables para almacenar información sobre la votación
+        self.voceros = set()  # Conjunto de IDs de voceros registrados
+        self.candidatos = {"Marlon": 0, "sandra": 0, "Stiwar": 0, "Blanco": 0}  # Diccionario para almacenar votos de cada candidato
+        self.votosRealizados = set()  # Conjunto de IDs de votos ya registrados
 
     def registrarVocero(self, idVocero):
+        # Método para registrar a un vocero
         self.voceros.add(idVocero)
         print(f"Vocero {idVocero} registrado exitosamente.")
 
     def votar(self, idVocero, candidato):
+        # Método para procesar un voto
         if idVocero not in self.votosRealizados:
             if candidato in self.candidatos:
+                # Incrementa el contador de votos para el candidato seleccionado
                 self.candidatos[candidato] += 1
+                # Registra el ID del votante para evitar votos duplicados
                 self.votosRealizados.add(idVocero)
                 print(f"Voto de {idVocero} registrado para {candidato}.")
             else:
@@ -20,10 +25,12 @@ class Votacion:
             print(f"{idVocero}, ya has votado.")
 
     def mostrarResultados(self):
+        # Método para mostrar los resultados de la votación
         print("\nResultados de la votación:")
         for candidato, votos in self.candidatos.items():
             print(f"{candidato}: {votos} votos")
 
+        # Determina al ganador y maneja empates
         ganador = max(self.candidatos, key=self.candidatos.get)
         empate = [candidato for candidato, votos in self.candidatos.items() if votos == self.candidatos[ganador] and candidato != "Blanco"]
 
@@ -32,16 +39,20 @@ class Votacion:
         else:
             print(f"\nEmpate entre {', '.join(empate)} con {self.candidatos[ganador]} votos cada uno!")
 
+# Crear una instancia de la clase Votacion
 votacion = Votacion()
 
+# Registro de voceros
 idVocero1 = input("Ingrese el ID del primer vocero: ")
 votacion.registrarVocero(idVocero1)
 
 idVocero2 = input("Ingrese el ID del segundo vocero: ")
 votacion.registrarVocero(idVocero2)
 
-idVocero3 = input("Ingrese el ID del segundo vocero: ")
+idVocero3 = input("Ingrese el ID del tercer vocero: ")
 votacion.registrarVocero(idVocero3)
+
+# Proceso de votación
 while True:
     idVocero = input("Ingrese su ID para votar (o 'fin' para terminar): ")
     if idVocero.lower() == 'fin':
@@ -53,4 +64,6 @@ while True:
     else:
         print("Vocero no registrado. Regístrese primero.")
 
+# Mostrar resultados al finalizar la votación
 votacion.mostrarResultados()
+
